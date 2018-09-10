cb0d68b8a64e90584ae40f31f180e31ed9158c2b
jdhenao <henao900@gmail.com>
2018-09-10 02:01:30 -0500

    Modificación de credenciales para mlab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
+    db: process.env.MONGODB_URI || 'mongodb://david:Fidelio2018@ds018498.mlab.com:18498/gestor_forms',
     SECRET_TOKEN: 'miclavedetokens'
   }
